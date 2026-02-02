376fc13929f0dceb952e5da8da35e4b806865669
DeepakMS-027 <deepaksaini149ms@gmail.com>
2026-02-02 21:19:14 +0530

    done

---
+++ Link_List/Intro.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+  public:
+  int data;
+  Node* next;
+  Node(int val){
+    data=val;
+  }
+};
+class List
+{
+  
+};
+
+int main()
+{
+
+
+return 0;
+}
