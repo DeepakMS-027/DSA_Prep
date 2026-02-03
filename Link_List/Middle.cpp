afb09e654cc3f66faf6ab30f3134d46cf52fc1e7
DeepakMS-027 <deepaksaini149ms@gmail.com>
2026-02-03 14:51:55 +0530

    middle of link list

---
+++ Link_List/Middle.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+  public:
+  int data;
+  Node* next;
+  Node(int val)
+  {
+    data= val;
+    next=NULL;
+  }
+};
+
+class List
+{
+  public:
+  Node* head;
+  Node* tail;
+  List()
+  {
+    head=tail=NULL;
+  }
+
+  void push_back(int val)
+  {
+    Node* newNode= new Node(val);
+  
+    if(head==NULL)
+    {
+      head=tail=newNode;
+      return ;
+    }
+    else{
+      tail->next=newNode;
+      tail= newNode;
+      return ;
+    }
+  }
+
+  void mid()
+  {
+    Node* slow=head;
+    Node* fast=head;
+    while(fast!=NULL && fast->next !=NULL)
+    {
+      slow=slow->next;
+      fast=fast->next->next;
+    }
+    cout<<slow->data;
+
+  }
+
+  void print ()
+  {
+    Node* temp=head;
+    while(temp!=NULL)
+    {
+      cout<<temp->data<<",";
+      temp=temp->next;
+    }
+  }
+
+};
+
+int main()
+{
+  List ll;
+
+  ll.push_back(1);
+  ll.push_back(2);
+  ll.push_back(3);
+  ll.push_back(4);
+  ll.push_back(5);
+  ll.push_back(6);
+  ll.push_back(7);
+  // ll.print();
+  ll.mid();
+
+return 0;
+}
