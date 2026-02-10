14a1a84f7a900e35b6825314f4afa2a2cb896fc1
DeepakMS-027 <deepaksaini149ms@gmail.com>
2026-02-10 22:31:54 +0530

    cycle

---
+++ Link_List/cycle.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    // bool hasCycle(ListNode *head) {
+    //     ListNode *slow=head;
+    //     ListNode *fast=head;
+    //     while(fast!=NULL&&fast->next!=NULL)
+    //     {
+    //         fast=fast->next->next;
+    //         slow=slow->next;
+    //         if(fast==slow){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+    // ListNode *detectCycle(ListNode *head) {
+    //     ListNode *Slow=head;
+    //     ListNode *Fast=head;
+    //     bool iscycle=false;
+    //     while(Fast!=NULL && Fast->next!=NULL)
+    //     {
+    //         Slow=Slow->next;
+    //         Fast=Fast->next->next;
+    //         if(Fast==Slow)
+    //         {
+    //             iscycle=true;
+    //             break;
+    //         }
+    //     }
+    //     if (iscycle==false)
+    //         {
+    //             return NULL;
+    //         }   
+    //     Slow=head;
+    //     while(Slow!=Fast)
+    //     {
+    //         Slow=Slow->next;
+    //         Fast=Fast->next;
+    //     }
+    //     return Slow;
+    // }
+};
+int main()
+{
+//code for cycle detection
+//code for cycle correction
+
+return 0;
+}
