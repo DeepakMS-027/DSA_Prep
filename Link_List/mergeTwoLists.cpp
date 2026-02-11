46feed1ba77f6a64b021de88d987b991f1061883
DeepakMS-027 <deepaksaini149ms@gmail.com>
2026-02-11 21:23:46 +0530

    mergeTwoLists

---
+++ Link_List/mergeTwoLists.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    // ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
+    //     if(head1==NULL||head2==NULL)
+    //     {
+    //         return head1 == NULL ? head2 : head1;
+    //     }
+    //     //case 1
+    //     if(head1->val <=head2->val)
+    //     {
+    //         head1->next=mergeTwoLists(head1->next,head2);
+    //         return head1;
+    //     }
+    //     //case2
+    //     else
+    //     {
+    //         head2->next=mergeTwoLists(head1,head2->next);
+    //         return head2;
+    //     }
+    // }
+};
+
+int main()
+{
+
+  //code for merge 2 sorted list
+
+return 0;
+}
