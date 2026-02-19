cf340fd83ac8c01ef91126e6af9d58d403a5ae77
DeepakMS-027 <deepaksaini149ms@gmail.com>
2026-02-19 20:53:44 +0530

    Valid Parentheses

---
+++ Link_List/Valid_Parentheses.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> data;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                data.push(s[i]);
+            } else {
+                if (data.size() == 0) {
+                    return false;
+                }
+
+                if (s[i] == ')' && data.top() == '(' ||
+                    s[i] == '}' && data.top() == '{' ||
+                    s[i] == ']' && data.top() == '[') {
+                    data.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return (data.size() == 0);
+    }
+};
+int main()
+{
+ Solution s;
+ cout<<s.isValid("{{([])[]}}")<<endl;
+ cout<<s.isValid("{([])[]}}")<<endl;
+ 
+
+return 0;
+}
